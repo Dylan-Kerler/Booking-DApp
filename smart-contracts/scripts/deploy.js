b0227f0cbf991ddd9db1d001bd90e91a0a96fca1
Dylan-Kerler <dylanphoon@gmail.com>
2021-03-27 11:47:51 +0000

    Add companies to deploy script

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,7 +1,15 @@
+const COMPANIES = {
+    cola: { id: 1, maxRooms: 10 },
+    pepsi: { id: 2, maxRooms: 10 },
+};
+
 async function main() {
     const Bookings = await ethers.getContractFactory("Bookings");
     const bookings = await Bookings.deploy();
-  
+
+    await bookings.addCompany(COMPANIES.cola.id, COMPANIES.cola.maxRooms);
+    await bookings.addCompany(COMPANIES.pepsi.id, COMPANIES.pepsi.maxRooms);
+
     console.log("bookings deployed to:", bookings.address);
 }
   
